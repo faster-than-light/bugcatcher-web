ceff139432948e6e0b31bd7a28842fdb7842ac00
Reuven <retzion@gmail.com>
2019-08-13 22:12:47 -0600

    Put Sample Results button above tour video

--- src/views/Tour.js
+++ src/views/Tour.js
@@ -14,14 +14,15 @@ export default class Tour extends Component {
       <Menu />
       <section id="tools" className="contents">
         <div className="tools-box">
+          <h2>See the Sample Results</h2>
+          <p>
+            <StlButton semantic primary href={resultsPdf}>Open Results PDF</StlButton>
+          </p>
+          <hr />
+          
           <h1>See BugCatcher In Action!</h1>
           <p><iframe width="100%" height="420" src="https://www.youtube.com/embed/5iocVVWZ4G0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
 
-          <h3>See the Sample Results PDF</h3>
-          <p>
-            <StlButton semantic href={resultsPdf}>Open Results</StlButton>
-          </p>
-          
           <h3>Additional Information</h3>
           <p>For more information about the tools used and languages supported, please visit our <Link to={'/tests'} onClick={() => {window.scrollTo({top: 0})}}>Tests</Link> page.</p>
         </div>
