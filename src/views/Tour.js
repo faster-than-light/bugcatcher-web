98a9a1ef8d47376297cb03837f9f73cf64944fa9
Reuven <retzion@gmail.com>
2019-08-14 16:05:11 -0600

    Add an H1 header to Tour view > "BugCatcher Tour"

--- src/views/Tour.js
+++ src/views/Tour.js
@@ -14,14 +14,17 @@ export default class Tour extends Component {
       <Menu />
       <section id="tools" className="contents">
         <div className="tools-box">
-          <h2>See the Sample Results</h2>
+          <h1>BugCatcher Tour</h1>
+
+          <h3>See the Sample Results</h3>
           <p>
             <StlButton semantic primary href={resultsPdf}>Open Results PDF</StlButton>
           </p>
           <hr />
           
-          <h1>See BugCatcher In Action!</h1>
+          <h3>See BugCatcher In Action!</h3>
           <p><iframe width="100%" height="420" src="https://www.youtube.com/embed/5iocVVWZ4G0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></p>
+          <hr />
 
           <h3>Additional Information</h3>
           <p>For more information about the tools used and languages supported, please visit our <Link to={'/tests'} onClick={() => {window.scrollTo({top: 0})}}>Tests</Link> page.</p>
